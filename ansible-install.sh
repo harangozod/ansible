4c42029ac9274c9f1bc16b5974bd6edfa0c0c526
harangozod <36441645+harangozod@users.noreply.github.com>
2018-02-13 12:43:18 +0100

    Update ansible-install.sh

--- ansible-install.sh
+++ ansible-install.sh
@@ -4,5 +4,8 @@ sudo apt-get install software-properties-common -y
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install ansible -y
-
 #sudo apt-get install openssh-server -y
+
+git clone https://github.com/andreipak/wordpress-ansible
+cd wordpress-ansible
+sudo ansible-playbook playbook.yml -i hosts -e mysql_root_password=#password#
